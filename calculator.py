05c4e01711d23ad4ae6d57f0bd9f379b0dc2ec16
shanks20071 <131152743+shanks20071@users.noreply.github.com>
2023-04-23 13:07:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print("choose your first number")
+
+a=int(input())
+print("now choose your second number")
+b=int(input())
+
+print("working on it\n")
+
+
+print("sum",(a+b))
+print("subtract",(a-b))
+print("floor division",(a//b))
+print("multiply",(a*b))
+
+print("exit or not")
+
+input("yes")
+print("exit")
